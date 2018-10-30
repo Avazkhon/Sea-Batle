2bb9c336d0b71b2ad0ab8bf3c5111db727445323
Avazkhon <Kamalxanovavazxon@gmail.come>
2018-10-30 14:24:56 +0700

    Start game

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Script add");
+const seaBatle = [
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,
+]
+
+// отрисовка блоков  
+for(let i = 0; i < 100; i++ ) {
+		document.getElementById('user').innerHTML += `<div class="seaMill " value="${seaBatle[i]}"></div>`;
+}
+
+function start() {
+		let num = Math.floor(Math.random() * (0,  99)) + 1;
+		seaBatle[num] = 4;
+		console.log(seaBatle);
+}
