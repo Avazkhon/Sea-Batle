862ed8b6eda25e6c3e771cd6a6a2b6738734e0fb
Avazkhon <Kamalxanovavazxon@gmail.come>
2018-11-04 10:14:23 +0700

    location automation

--- script.js
+++ script.js
@@ -34,15 +34,12 @@ const coordinates = [
 	'90', '91', '92', '93', '94', '95', '96', '97', '98', '99',
 ]
 
-// отрисовка блоков 
-for(let i = 0; i < 100; i++ ) {
-	document.getElementById('user').innerHTML += `<div class='seaMill' id='coordinates${coordinates[i]}'></div>`;
-}
 		// clsss для поиска свободного места в коорденате seaBatle
 class ShipDeck {
 
-	constructor(asd) {
-		this.asd = asd;
+	constructor(value, seaMillN) {
+		this.value = value;
+		this.seaMillN = seaMillN;
 	}
 
 	sayDeck () {
@@ -51,96 +48,60 @@ class ShipDeck {
 			seaBatle[num] == 0 &&
 			seaBatle[num + 1] == 0 &&
 			seaBatle[num - 1] == 0 &&
-			seaBatle[num - 9] == 0 &&
 			seaBatle[num + 9] == 0 &&
+			seaBatle[num - 9] == 0 &&
 			seaBatle[num + 10] == 0 &&
 			seaBatle[num - 10] == 0 &&
-			seaBatle[num - 11] == 0 &&
-			seaBatle[num + 11] == 0 
-			) 
-		{
-			seaBatle[num] = shipD.deck;
-			document.getElementById(`coordinates${coordinates[num]}`).innerHTML += "<div class='seaMillD'></div>";
+			seaBatle[num + 11] == 0 &&
+			seaBatle[num - 11] == 0
+			) {
+			seaBatle[num] = this.value;
+			document.getElementById(`coordinates${coordinates[num]}`).innerHTML += `<div class='${this.seaMillN}'></div>`;
+			if(shipD.deck == 2) {
+
+			}
 		}else {
 			this.sayDeck();
 		}
 	}
+
 }
 
+// отрисовка блоков 
+for(let i = 0; i < 100; i++ ) {
+	document.getElementById('user').innerHTML += `<div class='seaMill' id='coordinates${coordinates[i]}'></div>`;
+}
 
 // страт и размищение кораблей
 function start (){
-	let deck10 = 10;
-	let deck20 = 20;
-	let deck30 = 30;
 	reset++
 		// перезагрузка страницы если корабли уже размещены. проверка не очевидна но пока сойдет
 	if(reset > 2) {
 		document.location.reload();
 	}
-	// for определяет кол-во палуб
+	// for определяет кол-во палуб и кораблей
 	for(let i = 1; i <= 1; i ++) {
-		// рандом определяет коорденаты
-		const numA = Math.floor(Math.random() * (0,  99));
-		// проверка наличия корабля в коорденатях
-		if(seaBatle[numA] == 0) {
-			// это понадобиться для определени  статуса корабля
-			seaBatle[numA] = shipA.deck;
-			// размещения кораблей 
-			document.getElementById(`coordinates${coordinates[numA]}`).innerHTML += "<div class='seaMillA'></div>";
-				// размещение deck 
-			if(seaBatle[numA + deck10 ] == 0 ){
-				// это бонадобиться для определени  статуса корабля
-				seaBatle[numA + deck10 ] = shipA.deck;
-				// размещения deck 
-				document.getElementById(`coordinates${coordinates[numA + deck10]}`).innerHTML += "<div class='seaMillA'></div>";
-
-				if(seaBatle[numA + deck20 ] == 0){
-					seaBatle[numA + deck20 ] = shipA.deck;
-					document.getElementById(`coordinates${coordinates[numA + deck20]}`).innerHTML += "<div class='seaMillA'></div>";
-
-					if(seaBatle[numA + deck30 ] == 0) {
-						seaBatle[numA + deck30 ] = shipA.deck;
-						document.getElementById(`coordinates${coordinates[numA + deck30]}`).innerHTML += "<div class='seaMillA'></div>";
-					}
-				}
-			}
-		} 
-
+		// присваевает deck кораблю class
+		let item  = shipA.deck;
+		let shipDeckA = new ShipDeck(item, 'seaMillA');
+		shipDeckA.sayDeck();
 	};
 
 	for(let i = 1; i <= 2; i ++) {
-		const numB = Math.floor(Math.random() * (0,  99)) + 1;
-		if(seaBatle[numB] == 0) {
-			seaBatle[numB] = shipB.deck;
-			document.getElementById(`coordinates${coordinates[numB]}`).innerHTML += "<div class='seaMillB'></div>";
-			if(seaBatle[numB + deck10 ] == 0 ){
-				seaBatle[numB + deck10 ] = shipA.deck;
-				document.getElementById(`coordinates${coordinates[numB + deck10]}`).innerHTML += "<div class='seaMillB'></div>";
-
-				if(seaBatle[numB + deck20 ] == 0){
-					seaBatle[numB + deck20 ] = shipA.deck;
-					document.getElementById(`coordinates${coordinates[numB + deck20]}`).innerHTML += "<div class='seaMillB'></div>";
-				}
-			}
-		}
+			let item  = shipB.deck;
+			let shipDeckB = new ShipDeck(item, 'seaMillB');
+			shipDeckB.sayDeck();
 	}
 
 	for(let i = 1; i <= 3; i ++) {
-		const numC = Math.floor(Math.random() * (0,  99)) + 1;
-		if(seaBatle[numC] == 0) {
-			seaBatle[numC] = shipC.deck;
-			document.getElementById(`coordinates${coordinates[numC]}`).innerHTML += "<div class='seaMillC'></div>";
-			if(seaBatle[numC + deck10 ] == 0 ){
-				seaBatle[numC + deck10 ] = shipA.deck;
-				document.getElementById(`coordinates${coordinates[numC + deck10]}`).innerHTML += "<div class='seaMillC'></div>";
-			}
-		}
+			let item  = shipC.deck;
+			let shipDeckD = new ShipDeck(item, 'seaMillC');
+			shipDeckD.sayDeck();
 	}
 
 	for(let i = 1; i <= 4; i ++) {
 		let item  = shipD.deck;
-		let shipDeckD = new ShipDeck(item);
+		let shipDeckD = new ShipDeck(item, 'seaMillD');
 		shipDeckD.sayDeck();
 	}
 	console.log(seaBatle);
